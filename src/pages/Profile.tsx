aa8db7b33fa9bcb2d582f0a4cd3c2cd33c8ba667
x-anish-y <anishsasmal5481@gmail.com>
2026-01-03 13:31:47 +0530

    Update Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -41,7 +41,7 @@ export default function Profile() {
   const formatCurrency = (amount: number) => {
     return new Intl.NumberFormat('en-US', {
       style: 'currency',
-      currency: 'USD',
+      currency: 'INR',
       minimumFractionDigits: 0,
     }).format(amount);
   };
